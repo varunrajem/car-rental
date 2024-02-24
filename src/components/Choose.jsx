356c35c3b622908e4e66b958904f521ffaaa88c8
Varunraje <varunraje47@gmail.com>
2024-02-24 08:37:18 +0530

    choose card

---
+++ src/components/Choose.jsx
@@ -0,0 +1,23 @@
+const Choose = (props) => {
+  return (
+    <>
+      <div className='flex justify-center items-center h-40'>
+        <div className='w=[30%]'>
+          <div className="flex justify-center items-center bg-blue-500 p-6 rounded-sm">
+            {props.icon}
+          </div>
+
+        </div>
+        <div className='w-[70%]'>
+          <div className="px-2">
+            <p className="font-bold">{props.title}</p>
+            <p className="text-sm">{props.desc}</p>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Choose
