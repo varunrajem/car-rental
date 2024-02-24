f4830660a76ea050f79b4f5575838744a48ae4fe
Varunraje <varunraje47@gmail.com>
2024-02-24 08:36:37 +0530

    Inventoy Card Updated

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,4 +1,7 @@
 import Image from "next/image"
+import { MdAirlineSeatReclineExtra } from "react-icons/md";
+import { BsFillFuelPumpFill } from "react-icons/bs";
+import { IoIosSpeedometer } from "react-icons/io";
 
 const Inventory = (props) => {
   return (
@@ -7,11 +10,22 @@ const Inventory = (props) => {
         <Image src={props.pic} alt="Image" className="p-2 rounded-lg hover:scale-105 transition duration-500 cursor-pointer" />
         <div className="px-4">
           <p className="text-md font-bold">{props.name}</p>
-          <div>
-
+          <div className="flex justify-between py-1">
+            <div className="flex items-center">
+              <MdAirlineSeatReclineExtra size={22} className="text-blue-500" />
+              <p>{props.seats}</p>
+            </div>
+            <div className="flex items-center">
+              <BsFillFuelPumpFill size={22} className="text-blue-500" />
+              <p>{props.fuel}</p>
+            </div>
+            <div className="flex items-center">
+              <IoIosSpeedometer size={22} className="text-blue-500" />
+              <p>{props.speed}</p>
+            </div>
           </div>
           <div className="flex justify-around items-center border-t border-gray-600 py-3">
-            <div className="font-bold text-sm">3.32 Crore</div>
+            <div className="font-bold text-sm">{props.price}</div>
             <button className="bg-blue-500 px-6 py-1 rounded-md">Book Now</button>
           </div>
         </div>
